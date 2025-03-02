ae82a2addbbd1245e295b66e3b6c91c11747d707
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-03-02 16:36:25 +0600

    add

---
+++ exam-2.sql
@@ -0,0 +1,26 @@
+
+ SELECT last_name,LENGTH('last_name')
+ from employees
+ where last_name LIKE 'J%'
+     or last_name like 'A%'
+     or last_name like 'M%';
+  
+--  question-2
+
+  select last_name, salary, lpad(salary,15,'$') as SALARY
+  from employees;
+  
+  question --3
+  
+  select last_name as earns, salary as monthly, salary*3 as "Dream salary"
+  from employees;
+  
+  
+  question--4
+  select last_name ,hire_date,salary
+  from employees;
+  where hire_date< 
+  
+  
+  
+  
